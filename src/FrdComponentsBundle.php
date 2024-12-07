c4d36652ba961419033bfac5256f329cddb522ce
Ondrej Vana <vana.ondrej@gmail.com>
2024-12-06 17:51:04 -0800

    refactor: reorganize asset controller files

--- src/FrdComponentsBundle.php
+++ src/FrdComponentsBundle.php
@@ -1,12 +1,44 @@
 <?php
 namespace Frd\ComponentsBundle;
 
+use Symfony\Component\AssetMapper\AssetMapperInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class FrdComponentsBundle extends AbstractBundle
 {
-    public function getPath(): string
+    // public function getPath(): string
+    // {
+    //     return \dirname(__DIR__);
+    // }
+    public function prependExtension(ContainerConfigurator $configurator, ContainerBuilder $container): void
     {
-        return \dirname(__DIR__);
+        if (!$this->isAssetMapperAvailable($container)) {
+            return;
+        }
+
+        $container->prependExtensionConfig('framework', [
+            'asset_mapper' => [
+                'paths' => [
+                    __DIR__ . '/../assets' => '@frd/components-bundle',
+                ],
+            ],
+        ]);
+    }
+
+    private function isAssetMapperAvailable(ContainerBuilder $container): bool
+    {
+        if (!interface_exists(AssetMapperInterface::class)) {
+            return false;
+        }
+
+        // check that FrameworkBundle 6.3 or higher is installed
+        $bundlesMetadata = $container->getParameter('kernel.bundles_metadata');
+        if (!isset($bundlesMetadata['FrameworkBundle'])) {
+            return false;
+        }
+
+        return is_file($bundlesMetadata['FrameworkBundle']['path'] . '/Resources/config/asset_mapper.php');
     }
 }
